7d8d9d46fe63d387d91ab37f6487c5c379598f3f
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-05 18:16:19 +0100

    setup dynamic portfolio items

---
+++ assets/js/data.js
@@ -0,0 +1,46 @@
+//create a fortfolio item object
+
+export const portfolioItem = [
+  {
+    id: 1,
+    title: "Portfolio Item 1",
+    category: "Web Design",
+    img: "assets/img/1.svg",
+    url: "https://www.google.com",
+  },
+  {
+    id: 2,
+    title: "Portfolio Item 2",
+    category: "Web Design",
+    img: "assets/img/2.svg",
+    url: "https://www.google.com",
+  },
+  {
+    id: 3,
+    title: "Portfolio Item 3",
+    category: "Web Design",
+    img: "assets/img/3.svg",
+    url: "https://www.google.com",
+  },
+  {
+    id: 4,
+    title: "Portfolio Item 4",
+    category: "Web Design",
+    img: "assets/img/4.svg",
+    url: "https://www.google.com",
+  },
+  {
+    id: 5,
+    title: "Portfolio Item 5",
+    category: "Web Design",
+    img: "assets/img/5.svg",
+    url: "https://www.google.com",
+  },
+  {
+    id: 6,
+    title: "Portfolio Item 6",
+    category: "Web Design",
+    img: "assets/img/6.svg",
+    url: "https://www.google.com",
+  },
+];
