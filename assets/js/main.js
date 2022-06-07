d551995ce82c0ad38cb21baac0c1b20384e9eecb
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-07 23:41:55 +0100

    update stmp

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,11 @@ const navMenu = document.getElementById("nav-menu"),
 /*=============== PRELOADER ===============*/
 const preloader = document.getElementById("preloader");
 
+//wait until the document is loaded
+window.addEventListener("load", () => {
+  //hide preloader
+  preloader.style.display = "none";
+});
 
 /*=============== AUTO TYPING ===============*/
 const infoText = document.querySelector(".info-text");
