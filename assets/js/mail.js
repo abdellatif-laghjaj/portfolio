46b47681f66d265a468f7f0026467edaf44147bf
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-07 22:29:56 +0100

    setup stmp js

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,23 +1,48 @@
 // get all input data
-const form = document.querySelector('form');
-const email = document.querySelector('#email');
-const subject = document.querySelector('#subject');
-const message = document.querySelector('#message'); 
+const form = document.querySelector(".conatct-form");
+const email = document.getElementById("email");
+const name = document.getElementById("name");
+const message = document.getElementById("message");
 
 // add event listener to form
-form.addEventListener('submit', (e) => {
-    console.log('Form submitted');
-    e.preventDefault();
-    // check if inputs are empty
-    if(email.value === '' || subject.value === '' || message.value === ''){
-        // show error message
-        notie.alert({
-            type: 'error',
-            text: 'Please fill in all fields',
-            time: 2
-        });
-    } else {
-        window.location.href = 'mailto:${email.value}?subject=${subject.value}&body=${message.value}';
-        
-    }
+form.addEventListener("submit", (e) => {
+  console.log("Form submitted");
+  e.preventDefault();
+  // check if inputs are empty
+  if (email.value === "" || subject.value === "" || message.value === "") {
+    // show error message
+    notie.alert({
+      type: "error",
+      text: "Please fill in all fields",
+      time: 2,
+    });
+  } else {
+    // send email
+    sendMail(e, email.value, subject.value, message.value);
+    // show success message
+    notie.alert({
+      type: "success",
+      text: "Email sent successfully",
+      time: 2,
+    });
+  }
 });
+
+function sendMail(e, email, subject, message) {
+  const secrureToken = "00782b15-7e79-4762-9b31-f89f10cfb590";
+  const username = "abdelatiflaghjaj@gmail.com";
+  const password = "95B1E2CD6AEE9A023269BA097E6792ABF1FA";
+  const host = "smtp.elasticemail.com";
+  const my_email = username;
+  e.preventDefault();
+  Email.send({
+    SecureToken: secrureToken,
+    Host: host,
+    Username: username,
+    Password: password,
+    To: my_email,
+    From: email,
+    Subject: subject,
+    Body: message,
+  }).then((message) => alert(message));
+}
