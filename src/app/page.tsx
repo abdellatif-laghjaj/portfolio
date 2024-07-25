961c7243d8c5869fdd764eb634a87956845f078d
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-07-26 00:47:30 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 import PageBox from "@/components/core/PageBox";
-import { menuItems } from "@/data/navMenus";
+import {menuItems} from "@/data/navMenus";
 import HomeSection1 from "@/components/home/Section1";
 
 const HomeSection2 = dynamic(() => import("@/components/home/Section2"));
@@ -10,23 +10,23 @@ const HomeSection5 = dynamic(() => import("@/components/home/Section5"));
 const HomeSection6 = dynamic(() => import("@/components/home/Section6"));
 
 const FloatingNavbar = dynamic(() =>
-  import("@/components/navbar/FloatingNavbar")
+    import("@/components/navbar/FloatingNavbar")
 );
 const ScrollToTop = dynamic(() => import("@/components/common/ScrollToTop"));
 
 const Home = () => {
-  return (
-    <PageBox>
-      <FloatingNavbar className="app_nav" navItems={menuItems} />
-      <HomeSection1 id="hero" />
-      <HomeSection2 id="services" />
-      <HomeSection3 id="experiences" />
-      <HomeSection4 id="skills" />
-      <HomeSection5 id="projects" />
-      <HomeSection6 id="contact" />
-      <ScrollToTop />
-    </PageBox>
-  );
+    return (
+        <PageBox>
+            <FloatingNavbar className="app_nav" navItems={menuItems}/>
+            <HomeSection1 id="hero"/>
+            <HomeSection2 id="services"/>
+            <HomeSection3 id="experiences"/>
+            <HomeSection4 id="skills"/>
+            <HomeSection5 id="projects"/>
+            <HomeSection6 id="contact"/>
+            <ScrollToTop/>
+        </PageBox>
+    );
 };
 
 export default Home;
