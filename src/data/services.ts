376b750600761181addf38b0cb4c1222f170d726
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-07-26 01:39:32 +0100

    Update services.ts

--- src/data/services.ts
+++ src/data/services.ts
@@ -1,92 +1,90 @@
-import { IServiceItem } from "@/types";
+import {IServiceItem} from "@/types";
 
 const services: IServiceItem[] = [
-  {
-    id: 1,
-    title: "Mobile App Development",
-    icons: [
-      "/skills/socket-io.png",
-      "/skills/dart.svg",
-      "/skills/flutter.svg",
-      "/skills/getx.png",
-      "/skills/firebase.svg",
-    ],
-    shortDescription:
-      "I create engaging mobile applications for your audience.",
-    description:
-      "I create captivating mobile apps from concept to deployment for iOS and Android. Using cutting-edge technologies, I ensure seamless performance, intuitive interfaces, and robust functionality that align with your business goals. Enjoy a flawless user experience and outstanding results.",
-  },
-  {
-    id: 2,
-    title: "Web Development",
-    icons: [
-      "/skills/redux.svg",
-      "/skills/react.svg",
-      "/skills/nextjs.png",
-      "/skills/html.svg",
-      "/skills/css.svg",
-    ],
-    shortDescription: "I build visually stunning and user-friendly websites.",
-    description:
-      "I deliver stunning, user-friendly websites to establish your online presence. From simple sites to complex e-commerce platforms, I provide tailored solutions using the latest frameworks and technologies for a seamless, responsive, and SEO-friendly browsing experience. Enhance your online identity with quality.",
-  },
-  {
-    id: 3,
-    title: "Backend Development",
-    icons: [
-      "/skills/socket-io.png",
-      "/skills/docker.png",
-      "/skills/nodejs.svg",
-      "/skills/express.svg",
-      "/skills/aws.svg",
-    ],
-    shortDescription: "I create robust and scalable backend infrastructures.",
-    description:
-      "I enhance digital applications with robust, scalable backend infrastructures. I develop efficient database structures, APIs, and configure servers for optimal performance, security, and scalability, ensuring your applications handle high traffic and complex data management seamlessly. Rely on strong backend solutions.",
-  },
-  {
-    id: 4,
-    title: "Product Strategy",
-    icons: [
-      "/skills/git.svg",
-      "/images/collaboration.png",
-      "/images/logical-thinking.png",
-      "/images/analytical-skills.png",
-      "/skills/ubuntu.png",
-    ],
-    shortDescription:
-      "I define goals, target audiences, and roadmaps for success.",
-    description:
-      "I collaborate to define clear goals, target audiences, and a success roadmap. My expertise in product ideation and market analysis ensures your product meets user needs and aligns with your business strategy for long-term growth and full potential realization. Drive your product's success with strategic planning.",
-  },
-  {
-    id: 5,
-    title: "DevOps",
-    icons: [
-      "/skills/docker.svg",
-      "/skills/kubernetes.svg",
-      "/skills/aws.svg",
-      "/skills/jenkins.svg",
-      "/skills/terraform.svg",
-    ],
-    shortDescription: "I streamline development and operations processes.",
-    description:
-      "I streamline development and operations processes through effective DevOps practices. I implement continuous integration and deployment pipelines, manage cloud infrastructure, and use containerization to ensure efficient, reliable, and scalable software delivery. Improve your workflow with DevOps solutions.",
-  },
-  {
-    id: 6,
-    title: "Database Management",
-    icons: [
-      "/skills/mysql.svg",
-      "/skills/postgresql.svg",
-      "/skills/mongodb.svg",
-      "/skills/redis.svg",
-      "/skills/sqlite.svg",
-    ],
-    shortDescription: "I manage and optimize your database systems.",
-    description:
-      "I manage and optimize your database systems for performance, reliability, and scalability. With expertise in SQL and NoSQL databases, I design schemas, write complex queries, and implement best practices for data integrity and security. Ensure your data is managed effectively and efficiently.",
-  },
+    {
+        id: 1,
+        title: "Mobile App Development",
+        icons: [
+            "/skills/socket-io.png",
+            "/skills/dart.svg",
+            "/skills/flutter.svg",
+            "/skills/getx.png",
+            "/skills/firebase.svg",
+        ],
+        shortDescription: "Engaging mobile apps for iOS and Android.",
+        description:
+            "From concept to deployment, I create seamless mobile apps with cutting-edge technologies for an exceptional user experience.",
+    },
+    {
+        id: 2,
+        title: "Web Development",
+        icons: [
+            "/skills/redux.svg",
+            "/skills/react.svg",
+            "/skills/nextjs.png",
+            "/skills/html.svg",
+            "/skills/css.svg",
+        ],
+        shortDescription: "Visually stunning and user-friendly websites.",
+        description:
+            "I build responsive, SEO-friendly websites tailored to your needs, from simple sites to complex platforms.",
+    },
+    {
+        id: 3,
+        title: "Backend Development",
+        icons: [
+            "/skills/socket-io.png",
+            "/skills/docker.png",
+            "/skills/nodejs.svg",
+            "/skills/express.svg",
+            "/skills/aws.svg",
+        ],
+        shortDescription: "Robust and scalable backend infrastructures.",
+        description:
+            "I develop efficient backends with strong APIs, databases, and server configurations for high performance and scalability.",
+    },
+    {
+        id: 4,
+        title: "Product Strategy",
+        icons: [
+            "/skills/git.svg",
+            "/images/collaboration.png",
+            "/images/logical-thinking.png",
+            "/images/analytical-skills.png",
+            "/skills/ubuntu.png",
+        ],
+        shortDescription: "Defining goals and roadmaps for product success.",
+        description:
+            "I help define goals, target audiences, and strategic roadmaps to align your product with market needs and business goals.",
+    },
+    {
+        id: 5,
+        title: "DevOps",
+        icons: [
+            "/skills/docker.svg",
+            "/skills/kubernetes.svg",
+            "/skills/aws.svg",
+            "/skills/jenkins.svg",
+            "/skills/terraform.svg",
+        ],
+        shortDescription: "Streamlining development and operations.",
+        description:
+            "I implement DevOps practices to enhance workflows with CI/CD pipelines, cloud management, and containerization.",
+    },
+    {
+        id: 6,
+        title: "Database Management",
+        icons: [
+            "/skills/mysql.svg",
+            "/skills/postgresql.svg",
+            "/skills/mongodb.svg",
+            "/skills/redis.svg",
+            "/skills/sqlite.svg",
+        ],
+        shortDescription: "Optimizing and managing database systems.",
+        description:
+            "I design and optimize databases for performance and security, handling SQL and NoSQL systems for effective data management.",
+    },
 ];
 
 export default services;
