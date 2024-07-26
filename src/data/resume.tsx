52f4eb8f0366a2719634865f9f2b14e937e009ce
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-07-26 02:30:11 +0100

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,13 +2,12 @@ import {Icons} from "@/components/icons";
 import {CodeIcon, HomeIcon, NotebookIcon, PencilLine} from "lucide-react";
 
 export const DATA = {
-    name: "Dillion Verma",
+    name: "Abdellatif Laghjaj",
     initials: "DV",
     url: "https://dillion.io",
-    location: "San Francisco, CA",
+    location: "Taroudant, Morocco",
     locationLink: "https://www.google.com/maps/place/sanfrancisco",
-    description:
-        "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+    description: "Big Data and Artificial Intelligence Master Student, Full Stack Developer, and Tech Enthusiast from Morocco.",
     summary:
         "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
     avatarUrl: "/me.png",
