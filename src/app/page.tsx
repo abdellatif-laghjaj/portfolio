d3c585a626aedfb9ea1398545a5457c4dd6e7cbb
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-07-26 13:10:09 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,8 +30,8 @@ export default function Page() {
                             />
                         </div>
                         <BlurFade delay={BLUR_FADE_DELAY}>
-                            <Avatar className="size-28 border object-cover">
-                                <AvatarImage alt={DATA.name} src={DATA.avatarUrl}/>
+                            <Avatar className="size-28 border">
+                                <AvatarImage alt={DATA.name} src={DATA.avatarUrl} className="object-cover"/>
                                 <AvatarFallback>{DATA.initials}</AvatarFallback>
                             </Avatar>
                         </BlurFade>
