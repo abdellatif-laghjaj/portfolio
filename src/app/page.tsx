ab301871becef95fb9b1339a3172b2ad50bc80c4
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-07-26 02:49:27 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -166,10 +166,11 @@ export default function Page() {
                             <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                                 Want to chat? Just shoot me a dm{" "}
                                 <Link
-                                    href={DATA.contact.social.X.url}
+                                    href={DATA.contact.social.WhatsApp.url}
+                                    target="_blank"
                                     className="text-blue-500 hover:underline"
                                 >
-                                    with a direct question on twitter
+                                    with a direct question on WhatsApp
                                 </Link>{" "}
                                 and I&apos;ll respond whenever I can. I will ignore all
                                 soliciting.
