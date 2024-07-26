440c3a422b00fddd09674a52cea7b8d4da8d5f10
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-07-26 01:18:15 +0100

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,13 +1,13 @@
 const LocalConfig = {
-  values: {
-    TZ: process.env.TZ,
-    NODE_ENV: process.env.NODE_ENV,
-    AUTOPREFIXER_GRID: process.env.AUTOPREFIXER_GRID,
-    NEXT_PUBLIC_GTAG_ID: process.env.NEXT_PUBLIC_GTAG_ID,
-    NEXT_PUBLIC_GITHUB_TOKEN: process.env.NEXT_PUBLIC_GITHUB_TOKEN,
-    NEXT_PUBLIC_VERCEL_TOKEN: process.env.NEXT_PUBLIC_VERCEL_TOKEN,
-    NEXT_PUBLIC_RESUME_LINK: process.env.NEXT_PUBLIC_RESUME_LINK || "#",
-  },
+    values: {
+        TZ: process.env.TZ,
+        NODE_ENV: process.env.NODE_ENV,
+        AUTOPREFIXER_GRID: process.env.AUTOPREFIXER_GRID,
+        NEXT_PUBLIC_GTAG_ID: process.env.NEXT_PUBLIC_GTAG_ID,
+        NEXT_PUBLIC_GITHUB_TOKEN: process.env.NEXT_PUBLIC_GITHUB_TOKEN,
+        NEXT_PUBLIC_VERCEL_TOKEN: process.env.NEXT_PUBLIC_VERCEL_TOKEN,
+        NEXT_PUBLIC_RESUME_LINK: "./ABDELLATIF_LAGHJAJ_CV_EN.pdf",
+    },
 };
 
 export default LocalConfig;
