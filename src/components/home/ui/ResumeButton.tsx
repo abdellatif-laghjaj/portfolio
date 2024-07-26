9be681ed3e741d501a08930a9bb036743286f8b7
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-07-26 01:18:14 +0100

    Update ResumeButton.tsx

--- src/components/home/ui/ResumeButton.tsx
+++ src/components/home/ui/ResumeButton.tsx
@@ -2,15 +2,15 @@ import LocalConfig from "@/constants/config";
 import Link from "next/link";
 
 const ResumeButton = () => {
-  return (
-    <Link
-      className="app__outlined_btn min-w-[12rem]"
-      href={LocalConfig.values.NEXT_PUBLIC_RESUME_LINK}
-      target="_blank"
-    >
-      Download Resume
-    </Link>
-  );
+    return (
+        <Link
+            className="app__outlined_btn min-w-[12rem]"
+            href={LocalConfig.values.NEXT_PUBLIC_RESUME_LINK}
+            target="_blank"
+        >
+            Download Resume
+        </Link>
+    );
 };
 
 export default ResumeButton;
