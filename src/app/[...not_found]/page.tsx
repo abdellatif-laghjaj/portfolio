8ed06447a600b2fb5f1e12b36f69eaf41fcf012c
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-07-26 18:07:08 +0100

    Create page.tsx

---
+++ src/app/[...not_found]/page.tsx
@@ -0,0 +1,24 @@
+import BlurFade from "@/components/magicui/blur-fade";
+import {getBlogPosts} from "@/data/blog";
+import Link from "next/link";
+
+export const metadata = {
+    title: "Page Not Found",
+    description: "This page does not exist. Please check the URL and try again.",
+};
+
+const BLUR_FADE_DELAY = 0.04;
+
+export default async function BlogPage() {
+    const posts = await getBlogPosts();
+
+    return (
+        <section className="flex flex-col items-center justify-center h-full">
+            <BlurFade delay={BLUR_FADE_DELAY}>
+                <h1 className="font-medium text-2xl mb-8 tracking-tighter">
+                    Page Not Found
+                </h1>
+            </BlurFade>
+        </section>
+    );
+}
