d17b9be78cf8b706ab25ec7c25dd9eb992ddfd38
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-07-26 01:36:39 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import dynamic from "next/dynamic";
 import PageBox from "@/components/core/PageBox";
 import {menuItems} from "@/data/navMenus";
-import HomeSection1 from "@/components/home/Section1";
 
-const HomeSection2 = dynamic(() => import("@/components/home/Section2"));
-const HomeSection3 = dynamic(() => import("@/components/home/Section3"));
-const HomeSection4 = dynamic(() => import("@/components/home/Section4"));
-const HomeSection5 = dynamic(() => import("@/components/home/Section5"));
-const HomeSection6 = dynamic(() => import("@/components/home/Section6"));
+const Hero = dynamic(() => import("@/components/home/Hero"));
+const Services = dynamic(() => import("@/components/home/Services"));
+const Experiences = dynamic(() => import("@/components/home/Experiences"));
+const Skills = dynamic(() => import("@/components/home/Skills"));
+const Projects = dynamic(() => import("@/components/home/Projects"));
+const Contact = dynamic(() => import("@/components/home/Contact"));
 
 const FloatingNavbar = dynamic(() =>
     import("@/components/navbar/FloatingNavbar")
@@ -18,12 +18,12 @@ const Home = () => {
     return (
         <PageBox>
             <FloatingNavbar className="app_nav" navItems={menuItems}/>
-            <HomeSection1 id="hero"/>
-            <HomeSection2 id="services"/>
-            <HomeSection3 id="experiences"/>
-            <HomeSection4 id="skills"/>
-            <HomeSection5 id="projects"/>
-            <HomeSection6 id="contact"/>
+            <Hero id="hero"/>
+            <Services id="services"/>
+            <Experiences id="experiences"/>
+            <Skills id="skills"/>
+            <Projects id="projects"/>
+            <Contact id="contact"/>
             <ScrollToTop/>
         </PageBox>
     );
