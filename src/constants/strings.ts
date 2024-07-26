56db042ef657ef14c77ada97598896f1a97e109e
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-07-26 01:20:42 +0100

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,30 +1,34 @@
 class Strings {
-  static telegram = "Telegram";
-  static telegramLink = "https://telegram.me/nixrajput";
-  static telegramUsername = "nixrajput";
+    static telegram = "Telegram";
+    static telegramLink = "https://telegram.me/nixrajput";
+    static telegramUsername = "nixrajput";
 
-  static github = "GitHub";
-  static githubLink = "https://www.github.com/nixrajput";
-  static githubUsername = "nixrajput";
+    static whatsapp = "WhatsApp";
+    static whatsappLink = "https://wa.me/212636208825"
+    static whatsappUsername = "abdellatif-laghjaj";
 
-  static linkedIn = "LinkedIn";
-  static linkedInLink = "https://www.linkedin.com/in/nixrajput";
-  static linkedInUsername = "nixrajput";
+    static github = "GitHub";
+    static githubLink = "https://www.github.com/abdellatif-laghjaj";
+    static githubUsername = "abdellatif-laghjaj";
 
-  static instagram = "Instagram";
-  static instagramLink = "https://www.instagram.com/nixrajput";
-  static instagramUsername = "nixrajput";
+    static linkedIn = "LinkedIn";
+    static linkedInLink = "https://www.linkedin.com/in/abdellatif-laghjaj";
+    static linkedInUsername = "abdellatif-laghjaj";
 
-  static twitter = "Twitter";
-  static twitterLink = "https://www.twitter.com/nixrajput07";
-  static twitterUsername = "nixrajput07";
+    static instagram = "Instagram";
+    static instagramLink = "https://www.instagram.com/abdellatif.laghjaj";
+    static instagramUsername = "abdellatif.laghjaj";
 
-  static email = "Email";
-  static primaryEmailLink = "mailto:nkr.nikhil.nkr@gmail.com";
-  static primaryEmail = "nkr.nikhil.nkr@gmail.com";
+    static twitter = "Twitter";
+    static twitterLink = "https://www.twitter.com/nixrajput07";
+    static twitterUsername = "nixrajput07";
 
-  static fullName = "Nikhil Rajput";
-  static shortName = "nixrajput";
+    static email = "Email";
+    static primaryEmailLink = "mailto:abdelatiflaghjaj@gmail.com";
+    static primaryEmail = "abdelatiflaghjaj@gmail.com"
+
+    static fullName = "Abdellatif Laghjaj";
+    static shortName = "Abdellatif";
 }
 
 export default Strings;
