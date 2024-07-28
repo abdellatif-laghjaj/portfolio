c278fd31cd972c50baa74acd6152a3d7b231e6b3
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-07-28 17:19:16 +0100

    Update my-stats.tsx

--- src/components/my-stats.tsx
+++ src/components/my-stats.tsx
@@ -4,13 +4,7 @@ import React, {useEffect, useState} from "react";
 import {cn} from "@/lib/utils";
 import {DATA} from "@/data/resume";
 import {Icons} from "@/components/icons";
-
-interface Item {
-    name: string;
-    value: string;
-    icon: React.ReactNode;
-    color: string;
-}
+import StatCard from "@/components/stat-card";
 
 const calculateAge = (birthDate: Date) => {
     const ageDifMs = Date.now() - birthDate.getTime();
@@ -19,38 +13,6 @@ const calculateAge = (birthDate: Date) => {
 };
 
 const birthDate = new Date(DATA.birthDate);
-
-const Stat = ({name, value, icon, color}: Item) => {
-    return (
-        <figure
-            className={cn(
-                "relative w-full cursor-pointer overflow-hidden rounded-2xl p-4",
-                "transition-all duration-200 ease-in-out hover:scale-[103%]",
-                "bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
-                "transform-gpu dark:bg-transparent dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
-            )}
-        >
-            <div className="flex flex-row items-center gap-3">
-                <div
-                    className="flex size-10 items-center justify-center rounded-2xl"
-                    style={{
-                        backgroundColor: color,
-                    }}
-                >
-                    {icon}
-                </div>
-                <div className="flex flex-col overflow-hidden">
-                    <figcaption className="text-sm sm:text-lg font-medium dark:text-white">
-                        {name}
-                    </figcaption>
-                    <p className="text-lg font-bold dark:text-white/60">
-                        {value}
-                    </p>
-                </div>
-            </div>
-        </figure>
-    );
-};
 
 export function MyStats({
                             className,
@@ -135,7 +97,7 @@ export function MyStats({
         <div className={cn("w-full", className)}>
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                 {stats.map((item, idx) => (
-                    <Stat {...item} key={idx}/>
+                    <StatCard {...item} key={idx}/>
                 ))}
             </div>
         </div>
