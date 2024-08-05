42241c771d4cc04c854913623b7ac4320c8bf6ab
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2024-08-05 13:24:39 +0100

    Update page.tsx

--- src/app/[...not_found]/page.tsx
+++ src/app/[...not_found]/page.tsx
@@ -7,14 +7,12 @@ export const metadata = {
     description: "This page does not exist. Please check the URL and try again.",
 };
 
-const BLUR_FADE_DELAY = 0.04;
-
 export default async function BlogPage() {
     const posts = await getBlogPosts();
 
     return (
         <section className="flex flex-col items-center justify-center h-full">
-            <BlurFade delay={BLUR_FADE_DELAY}>
+            <BlurFade delay={DATA.BLUR_FADE_DELAY}>
                 <h1 className="font-medium text-2xl mb-8 tracking-tighter">
                     Page Not Found
                 </h1>
