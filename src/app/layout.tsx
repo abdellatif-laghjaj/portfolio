f6bf509aa4809b0e60d9adda366d77764357d537
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2025-08-25 23:45:00 +0100

    Switch font to Merriweather and update weights
    Replaces the Inter font with Merriweather for the main layout and specifies font weights 300, 400, and 700. This change updates the visual style of the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,13 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { DATA } from "@/data/resume";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
+import { Merriweather as FontSans } from "next/font/google";
 import "./globals.css";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
+  weight: ["300", "400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -59,7 +60,7 @@ export default function RootLayout({
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
-          fontSans.variable,
+          fontSans.variable
         )}
       >
         <ThemeProvider attribute="class" defaultTheme="light">
