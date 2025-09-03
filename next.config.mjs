a15ac3f08e2bf10d8e8913650de4754cc9c6472f
ABDELLATIF LAGHJAJ <abdelatiflaghjaj@gmail.com>
2025-09-03 11:35:44 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    staleTimes: {
+      dynamic: 30,
+      static: 180,
+    },
+    // Add this to disable CSS optimization temporarily
+    optimizeCss: false,
+  },
+  bundlePagesRouterDependencies: true,
+  outputFileTracingRoot: process.cwd(),
 };
-
 export default nextConfig;
